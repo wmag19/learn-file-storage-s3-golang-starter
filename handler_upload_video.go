2b101b85c3923e664e7817b3eb393937735392c9
Will Maguire <will_maguire99@protonmail.com>
2026-02-21 14:45:37 +0000

    split files by aspect ratio into different s3 bucket directories

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -37,15 +37,15 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 
 	fmt.Println("uploading file for video", videoID, "by user", userID)
-
 	const maxMemory = 1 << 30 //1GB limit
+
+	r.Body = http.MaxBytesReader(w, r.Body, maxMemory)
+
 	err = r.ParseMultipartForm(maxMemory)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "unable to parse form entry", err)
 		return
 	}
-	r.Body = http.MaxBytesReader(w, r.Body, maxMemory)
-	defer r.Body.Close()
 
 	dbVideo, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -70,19 +70,18 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusNotAcceptable, "Invalid file extension", err)
 	}
 
-	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	tempFile, err := os.CreateTemp("", "tubely-upload-*.mp4")
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Unable to save video", err)
 		return
 	}
-	defer os.Remove(tempFile.Name())
-	defer tempFile.Close()
 
 	_, err = io.Copy(tempFile, file)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Unable to save video", err)
 		return
 	}
+	tempFile.Sync()
 	_, err = tempFile.Seek(0, io.SeekStart)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Unable to read new file", err)
@@ -93,7 +92,25 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, "Unable to generate random file key for upload:", err)
 		return
 	}
-	fileName := randomKey + ".mp4"
+	defer tempFile.Close()
+
+	aspectRatio, err := getVideoAspectRatio(tempFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to calculate aspect ratio:", err)
+		return
+	}
+
+	var fileName string
+	switch aspectRatio { //Split file into different containers in s3 depending on aspect ratio
+	case "other":
+		fileName = "/other/" + randomKey + ".mp4"
+	case "16:9":
+		fileName = "/landscape/" + randomKey + ".mp4"
+	case "9:16":
+		fileName = "/portrait/" + randomKey + ".mp4"
+	default:
+		fileName = "/error/" + randomKey + ".mp4"
+	}
 
 	s3Options := &s3.PutObjectInput{
 		Body:        tempFile,
@@ -106,6 +123,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, "Unable to save to s3:", err)
 		return
 	}
+	defer os.Remove(tempFile.Name())
 
 	videoURL := "https://" + cfg.s3Bucket + ".s3." + cfg.s3Region + ".amazonaws.com/" + fileName
 
---
+++ video.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"os/exec"
+)
+
+type probeResult struct {
+	Streams []struct {
+		CodecType          string `json:"codec_type"`
+		Width              int    `json:"width"`
+		Height             int    `json:"height"`
+		DisplayAspectRatio string `json:"display_aspect_ratio"`
+	} `json:"streams"`
+}
+
+func getVideoAspectRatio(filepath string) (string, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filepath)
+	var outBuf bytes.Buffer
+	cmd.Stdout = &outBuf
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	var res probeResult
+	if err := json.Unmarshal(outBuf.Bytes(), &res); err != nil {
+		return "", err
+	}
+
+	var aspectRatio string
+
+	for i := range res.Streams {
+		if res.Streams[i].CodecType == "video" {
+			aspectRatio = res.Streams[i].DisplayAspectRatio
+			break
+		}
+	}
+	if aspectRatio != "16:9" && aspectRatio != "9:16" {
+		aspectRatio = "other"
+	}
+
+	return aspectRatio, nil
+}
